c2459c3f7eeed8ec27912c1fda40cda59ab1c238
erick garcia <erick garcia@DESKTOP-QFHIISE>
2023-03-08 17:42:54 -0600

    Agregar archivos de proyecto.

---
+++ TrabajoClasesVisualBasic/Module1.vb
@@ -0,0 +1,41 @@
+﻿Module Module1
+
+    Sub Main()
+        Try
+            Dim lista As List(Of contacto) = New List(Of contacto)()
+            Dim salir As Boolean = False
+            Console.WriteLine("¿Cuántos contactos desea guardar?")
+            Dim i As Integer = Integer.Parse(Console.ReadLine())
+            Dim a As Integer = 0
+            While Not salir
+                a += 1
+            End While
+
+            Dim x As contacto
+            x = New contacto()
+            Console.WriteLine("Nombre")
+            x.Nombre = Console.ReadLine()
+            Console.WriteLine("Telefono")
+            x.Telefono = Console.ReadLine()
+            Console.WriteLine("Fecha de Nacimiento")
+            x.FechaNacimiento = DateTime.Parse(Console.ReadLine())
+            Console.WriteLine("Favor de ingresar un numero de telefono")
+            x.Telefono = Console.ReadLine()
+            lista.Add(x)
+            If i = a Then
+                salir = True
+            End If
+
+            Console.Clear()
+            Console.WriteLine("Nombre--Edad--Numero de telefono")
+            For Each c As contacto In lista
+                Console.WriteLine(x.ToString())
+            Next
+        Catch e As FormatException
+            Console.WriteLine(e.Message)
+        End Try
+
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ TrabajoClasesVisualBasic/persona.vb
@@ -0,0 +1,57 @@
+﻿Public Class persona
+    Protected nombre As String
+    Protected fechadenacimiento As DateTime?
+    Protected correo As String
+    Public Property nombre_ As String
+        Get
+            Return nombre
+        End Get
+        Set(value As String)
+
+            nombre = value
+
+        End Set
+    End Property
+
+    Public Property fechadenacimiento_ As DateTime
+
+        Get
+            Return fechadenacimiento
+        End Get
+        Set(value As DateTime)
+
+            fechadenacimiento = value
+
+        End Set
+    End Property
+
+    Public ReadOnly Property edad() As String
+
+        Get
+            Dim edad_ As String
+            edad = (DateTime.Now.Year - fechadenacimiento.Value.Year)
+            Return edad
+        End Get
+    End Property
+
+    Sub persona(nombre_, fechadenacimiento_)
+        nombre = nombre_
+        fechadenacimiento = fechadenacimiento_
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return nombre.ToUpper + " " + edad
+    End Function
+
+    Public Property Correo_ As String
+        Get
+            Return correo
+        End Get
+        Set(value As String)
+            correo = value
+        End Set
+
+    End Property
+
+
+End Class
